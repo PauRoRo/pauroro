cfc42acf47afcf1d5c918609e1a547cf43e831fc
PauRoRo <74688114+PauRoRo@users.noreply.github.com>
2021-03-11 23:29:51 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -14,7 +14,6 @@ $carta .= "Mensaje: $mensaje";
 
 //Enviando Mensaje
 mail($destinatario, $asunto, $carta);
-header('Location: mensaje_exitoso.html');
-echo "<script>alert('Correo enviado')</script>";
+header('Location: mensaje_exitoso.html')
 //header('Location: English/message.html');
  ?>
