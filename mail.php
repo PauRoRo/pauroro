acdc9467b20a7ae32ce1610ca9de6d877fc10e31
PauRoRo <74688114+PauRoRo@users.noreply.github.com>
2021-03-12 15:09:37 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,25 +1,52 @@
 <?php
-// Llamando a los campos
+
 $nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$mensaje = $_POST['mensaje'];
+$correo= $_POST['correo'];
+$mensaje= $_POST['mensaje'];
 
-// Datos para el Correo
-$to = "paulina2807@hotmail.com";
-$asunto = "Contacto desde web Paula";
+$body = "Nombre: " . $nombre . "<br>Correo: " . $correo . "<br>Mensaje: " . $mensaje;
 
-$carta = "De: $nombre \n";
-$carta .= "Correo: $correo \n";
-$carta .= "Mensaje: $mensaje";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-//Enviando Mensaje
-// mail($to, $asunto, $carta);
-// header('Location:mensaje_exitoso.html');
-// header('Location:English/message.html')
-if($correo!=NULL){
-    mail($to,$asunto,$carta);
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'paulina2807g@gmail.com';                     //SMTP username
+    $mail->Password   = 'kingsandqueens30';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('paulina2807g@gmail.com', $nombre);
+    $mail->addAddress('paulina2807g@gmail.com');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Asunto muy importante';
+    $mail->Body    = $body;
+    $mail->CharSet = 'UTF-8';
+    $mail->send();
+    echo '<script>
+    alert("Mensaje se envió correctamente");
+    window.history.go(-1);
+    </script>';
+
+} catch (Exception $e) {
+    echo "Hubo error al enviar mensaje: {$mail->ErrorInfo}";
 }
-//redirect
-header("Location:mensaje_exitoso.html");
 
- ?>
+// // get data from form
+// mail($to,$subject,$txt,$headers);
+// //redirect
+// header("Location:mensaje.html");
+?>
