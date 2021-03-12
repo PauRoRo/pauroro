c74ca24d721caf0f7919581935de4bdc4ceb4640
PauRoRo <74688114+PauRoRo@users.noreply.github.com>
2021-03-12 00:07:19 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -7,16 +7,21 @@ $correo = $_POST['correo'];
 $mensaje = $_POST['mensaje'];
 
 // Datos para el Correo
-//$destinatario = "paulina2807@hotmail.com";
-$destinatario = "valerielucia0@gmail.com";
+$destinatario = "paulina2807@hotmail.com";
 $asunto = "Contacto desde web Paula";
 
 $carta = "De: $nombre \n";
 $carta .= "Correo: $correo \n";
 $carta .= "Mensaje: $mensaje";
 
+$host = $_SERVER['HTTP_HOST'];
+$ruta = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$html = 'mensaje_exitoso.html';
+$url = "http://$host$ruta/$html";
+
 //Enviando Mensaje
 mail($destinatario, $asunto, $carta);
-header('Location: mensaje_exitoso.html')
+header("Location: $url");
+//header('Location: mensaje_exitoso.html')
 //header('Location: English/message.html');
  ?>
