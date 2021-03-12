cc4b457dfa575286e5f396266e6d845005105786
PauRoRo <74688114+PauRoRo@users.noreply.github.com>
2021-03-12 17:25:43 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,59 +1,17 @@
 <?php
+$nombreCliente = $_POST['nombre'];
+$emailCliente  = $_POST['Correo'];
+$msjCliente    = $_POST['mensaje'];
 
-$nombre = $_POST['nombre'];
-$correo= $_POST['correo'];
-$mensaje= $_POST['mensaje'];
+$paraCliente    = $emailCliente;
+$tituloCliente  = "Mi Formulario de Contacto..";
+$mensajeCliente = "<html>".
 
-$body = "Nombre: " . $nombre . "<br>Correo: " . $correo . "<br>Mensaje: " . $mensaje;
+$cabecerasCliente  = 'MIME-Version: 1.0' . "\r\n";
+$cabecerasCliente .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$cabecerasCliente .= 'From: Bogota Colombia<paulina2807g@gmail.com>';
+$enviadoCliente   = mail($paraCliente, $tituloCliente, $mensajeCliente, $cabecerasCliente);
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+header('Location:mensaje_exitoso.html');
 
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
-
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'paulina2807g@gmail.com';                     //SMTP username
-    $mail->Password   = 'kingsandqueens30';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    //Recipients
-    $mail->setFrom('paulina2807g@gmail.com', $nombre);
-    $mail->addAddress('paulina2807g@gmail.com');     //Add a recipient
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Asunto muy importante';
-    $mail->Body    = $body;
-    $mail->SMTPOptions = array(
-            'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true
-            )
-          );
-    $mail->CharSet = 'UTF-8';
-    $mail->send();
-    echo '<script>
-    alert("Mensaje se envió correctamente");
-    window.history.go(-1);
-    </script>';
-
-} catch (Exception $e) {
-    echo "Hubo error al enviar mensaje: {$mail->ErrorInfo}";
-}
-
-// // get data from form
-// mail($to,$subject,$txt,$headers);
-// //redirect
-// header("Location:mensaje.html");
 ?>
